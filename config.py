63044d85164ba7b7637da47f56a97fea82126aa9
Lin En Tan <linentan04@gmail.com>
2026-02-09 13:00:33 -0600

    Fix errors and set configs to .env keys

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
-API_KEY = "FO4P4G9WA2QYIYON"
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Stock and News API keys
+S_API_KEY = os.getenv("STOCK_API_KEY")
+N_API_KEY = os.getenv("NEWS_API_KEY")
+
+# Twilio credentials
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+FROM_NUMBER = os.getenv("VIRTUAL_TWILIO_NUMBER")
+TO_NUMBER = os.getenv("VERIFIED_NUMBER")
+
+# Stock and company info
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+# API endpoints
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
